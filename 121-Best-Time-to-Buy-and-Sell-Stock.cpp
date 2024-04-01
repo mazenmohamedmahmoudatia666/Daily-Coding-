1d2db4d9c56d5484898dd9801edbe8f6f0c7d67a
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-04-01 21:40:36 +0200

    [121] [Time Beats: 94.22%] [Memory Beats: 37.61%] - LeetPush

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm> // For std::max
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int minPrice = INT_MAX;
+        
+        for (int price : prices) {
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+        
+        return maxProfit;
+    }
+};
