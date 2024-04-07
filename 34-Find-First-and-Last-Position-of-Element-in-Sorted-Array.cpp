7b0b6ed4059378de7bbb1da8cd9d9648a3b518cd
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-04-07 23:26:10 +0200

    [34] [Time Beats: 21.52%] [Memory Beats: 86.18%] - LeetPush

--- 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.cpp
+++ 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.cpp
@@ -4,20 +4,45 @@
 #include<climits>
 using namespace std;
 
+
 class Solution {
 public:
-	vector<int> searchRange(vector<int> &v, int target) {
-		auto p = equal_range(v.begin(), v.end(), target);
+	int BSfindFirst(vector<int> &nums, int val) {
+		int start = 0, end = (int) nums.size() - 1;
 
-		if (p.first == v.end())
-			return {-1, -1};
+		while (start < end) {
+			int mid = start + (end - start) / 2;
+			if (nums[mid] < val)
+				start = mid + 1;
+			else if (nums[mid] > val)
+				end = mid - 1;
+			else
+				end = mid;
+		}
+        if(start < 0 || start >= (int)nums.size())
+            return -1;
+		return nums[start] == val? start : -1;
+	}
 
-        int x = p.first - v.begin();    // first >=
-        int y = p.second - v.begin();   // first >   -1 to get last
+	int BSfindLast(vector<int> &nums, int val) {
+		int start = 0, end = (int) nums.size() - 1;
 
-        if (v[x] != target)
-            return {-1, -1};
+		while (start < end) {
+			int mid = start + (end - start + 1) / 2;
+			if (nums[mid] < val)
+				start = mid + 1;
+			else if (nums[mid] > val)
+				end = mid - 1;
+			else
+				start = mid;
+		}
+        if(start < 0 || start >= (int)nums.size())
+            return -1;
+		return nums[start] == val? start : -1;
+	}
 
-		return {x, y-1};
+	vector<int> searchRange(vector<int> &nums, int target) {
+		return {BSfindFirst(nums, target), BSfindLast(nums, target), };
 	}
 };
+
