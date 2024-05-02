0f8ca8d860c16c93667221c9969f5e5f61201d9a
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-05-02 16:27:29 +0300

    [153] [Time Beats: 59.16%] [Memory Beats: 59.27%] - LeetPush

--- 153-Find-Minimum-in-Rotated-Sorted-Array.cpp
+++ 153-Find-Minimum-in-Rotated-Sorted-Array.cpp
@@ -4,17 +4,7 @@ public:
         int n = nums.size();
         int l = 0, r = n - 1;
         int minn = *min_element(nums.begin(), nums.end());
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == minn)
-                return minn;
-            else if (nums[mid] < minn) {
-                l = mid + 1;
-            }
-            else {
-                r = mid - 1;
-            }
-        }
-        return minn; // If min element not found
+        
+        return minn; 
     }
 };
