418a8342dbe46d60f26df4743d21c4910ba8e4dd
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-07-04 20:43:55 +0300

    [392] [Time Beats: 47.39%] [Memory Beats: 32.35%] - LeetPush

--- 392-Is-Subsequence.cpp
+++ 392-Is-Subsequence.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        size_t pos = 0;  // Starting position in t
+        int sp = 0;
+        int tp = 0;
 
-        for (char c : s) {
-            pos = t.find(c, pos);  // Find c in t starting from pos
-            if (pos == string::npos) {
-                return false;  // If c is not found, return false
+        while (sp < s.length() && tp < t.length()) {
+            if (s[sp] == t[tp]) {
+                sp++;
             }
-            ++pos;  // Move to the next character in t
+            tp++;
         }
-        return true;  // All characters of s are found in t in order
+
+        return sp == s.length();        
     }
 };
