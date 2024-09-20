0602b604bfeb78537d6b2ef8dbf93025f2b86757
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-09-20 12:32:07 +0300

    [214] [Time Beats: 64.09%] [Memory Beats: 68.42%] - LeetPush

--- 214-Shortest-Palindrome.cpp
+++ 214-Shortest-Palindrome.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        int count = kmp(string(s.rbegin(), s.rend()), s);
-        return string(s.rbegin(), s.rend()).substr(0, s.length() - count) + s;
-    }
+        string rev_s = string(s.rbegin(), s.rend());  // Reverse of the string s
+        string combined = s + \#\ + rev_s;            // Combine s and its reverse with a separator
 
-private:
-    int kmp(const string &txt, const string &patt) {
-        string newString = patt + '#' + txt;
-        vector<int> pi(newString.length(), 0);
-        int i = 1, k = 0;
-        while (i < newString.length()) {
-            if (newString[i] == newString[k]) {
-                k++;
-                pi[i] = k;
-                i++;
-            } else {
-                if (k > 0) {
-                    k = pi[k - 1];
-                } else {
-                    pi[i] = 0;
-                    i++;
-                }
+        // Compute the prefix function for the combined string (KMP algorithm)
+        int n = combined.size();
+        vector<int> prefix(n, 0);
+
+        for (int i = 1; i < n; i++) {
+            int j = prefix[i - 1];
+            while (j > 0 && combined[i] != combined[j]) {
+                j = prefix[j - 1];
             }
+            if (combined[i] == combined[j]) {
+                j++;
+            }
+            prefix[i] = j;
         }
-        return pi.back();
+
+        // The length of the longest palindrome prefix is stored in the last value of prefix array
+        int longest_palindrome_prefix = prefix[n - 1];
+
+        // Append the reverse of the remaining part of s to the front
+        return rev_s.substr(0, s.size() - longest_palindrome_prefix) + s;
     }
 };
